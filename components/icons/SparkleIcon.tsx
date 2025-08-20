c7c0ceb56521f1705b70d58e892ebe6bd3ccfbaa
Shai javeed <116418517+shaikafridi557@users.noreply.github.com>
2025-08-20 19:51:24 +0530

    feat: Copy 'SparkleIcon.tsx'

---
+++ components/icons/SparkleIcon.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+const SparkleIcon: React.FC = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+        <path d="M12 3L9.5 8.5L4 11L9.5 13.5L12 19L14.5 13.5L20 11L14.5 8.5L12 3z"/>
+        <path d="M5 3v4"/>
+        <path d="M19 17v4"/>
+        <path d="M3 5h4"/>
+        <path d="M17 19h4"/>
+    </svg>
+);
+
+export default SparkleIcon;
